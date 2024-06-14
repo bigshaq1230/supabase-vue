7213d020cc2503fdb36ffb8bcdc5f4d412fe3fc1
ahmad <ahmad.ben.hamad2004@gmail.com>
2024-06-14 02:56:24 +0100

    start

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+
+
+      <nav>
+        <RouterLink to="/" class="link">Home</RouterLink>
+        <RouterLink to="/about" class="link">About</RouterLink>
+      </nav>
+  </header>
+  <br><br><br><br><br>
+  <RouterView />
+</template>
+<style>
+  .link {
+    margin: 1%
+  }
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+  <form>
+    <label for="email">Email: </label><input type="email" name="" id="email">
+    <button @click="">Sign In</button>
+  </form>
+
+</template>
+<style>
+  form * {
+    margin: 1%
+  }
+</style>
