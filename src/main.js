75d778c0dccfd44e5a6e08f46aaf45d3123cc807
ahmad <ahmad.ben.hamad2004@gmail.com>
2024-06-27 01:30:52 +0100

    fixed google api key

--- src/main.js
+++ src/main.js
@@ -4,10 +4,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/router'
 import vue3GoogleLogin from 'vue3-google-login'
-
+const google_api_key = import.meta.env.VITE_GOOGLE_API_KEY
 const app = createApp(App)
 app.use(router)
 app.use(vue3GoogleLogin, {
-    clientId: '703900486616-4o7kadp330cgd98b203luh1q3n0cnte1.apps.googleusercontent.com'
+    clientId: google_api_key
   })
 app.mount('#app')
